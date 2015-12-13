95ec80f78fd9b3bc06daef0ca1b64b43e940d019
Ta Thanh Dinh <tathanhdinh@gmail.com>
2015-12-13 01:01:03 +0100

    new fix

--- src/cfgrecon/src/graph/graph.cpp
+++ src/cfgrecon/src/graph/graph.cpp
@@ -200,7 +200,8 @@ static auto numbering_from_vertex (bb_vertex_desc_t current_vertex, bb_graph_t& 
 
   // deciding order of traversing
   std::stable_sort(
-        std::begin(next_unnumbered_vertices), std::end(next_unnumbered_vertices), [&graph](bb_vertex_desc_t va, bb_vertex_desc_t vb)
+        std::begin(next_unnumbered_vertices), std::end(next_unnumbered_vertices),
+        [&graph](bb_vertex_desc_t va, bb_vertex_desc_t vb)
   {
     return (std::get<BB_ADDRESSES>(graph[va]).front() < std::get<BB_ADDRESSES>(graph[vb]).front());
   });
@@ -502,14 +503,14 @@ auto save_basic_block_graph_to_dot_file (const std::string& filename) -> void
   std::ofstream output_file(filename.c_str(), std::ofstream::out | std::ofstream::trunc);
 
   // write thumbnail basic block graph (for sophisticated program: basic block too long to print out)
-//  boost::write_graphviz(output_file, graph,
-//                        std::bind(write_graph_thumbnail_vertex<cfg_or_tree>, std::placeholders::_1, std::placeholders::_2),
-//                        std::bind(write_cfg_edge, std::placeholders::_1, std::placeholders::_2));
+  boost::write_graphviz(output_file, graph,
+                        std::bind(write_graph_thumbnail_vertex<cfg_or_tree>, std::placeholders::_1, std::placeholders::_2),
+                        std::bind(write_cfg_edge, std::placeholders::_1, std::placeholders::_2));
 
   // write normal basic block graph
-  boost::write_graphviz(output_file, graph,
-                        std::bind(write_graph_vertex<cfg_or_tree>, std::placeholders::_1, std::placeholders::_2),
-                        std::bind(write_cfg_edge, std::placeholders::_1, std::placeholders::_2));
+//  boost::write_graphviz(output_file, graph,
+//                        std::bind(write_graph_vertex<cfg_or_tree>, std::placeholders::_1, std::placeholders::_2),
+//                        std::bind(write_cfg_edge, std::placeholders::_1, std::placeholders::_2));
 
   output_file.close();
   return;
